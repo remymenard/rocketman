713aca3e1c7ccd08e909c2fbac47f1c4fc33b2be
Remy Menard <remymenard.work@gmail.com>
2020-08-17 15:57:49 +0200

    Created all routes written on the google docs file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :rockets, only: [:index, :show] do
+    resources :orders, only: [:index, :new, :create]
+  end
+  resources :orders, only: [:index]
+
+  namespace :owners do
+    resources :orders, only: [:index] do
+      member do
+        patch 'accept'
+        patch 'decline'
+      end
+    end
+    resources :rockets, only: [:new, :create, :index, :destroy, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
