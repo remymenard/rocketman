8cd000243ca1f445b4e18f64969c2a61f96e8688
Remy Menard <remymenard.work@gmail.com>
2020-08-17 17:37:37 +0200

    added validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,13 @@ class Order < ApplicationRecord
   belongs_to :rocket
   belongs_to :renter, class_name: 'User'
 
+  validates :rocket, presence: true
+  validates :renter, presence: true
+
+  validates :begin_date, presence: true, allow_blank: false
+  validates :end_date, presence: true, allow_blank: false
+
+  validates :total_price, numericality: { only_integer: true }
+
 
 end
