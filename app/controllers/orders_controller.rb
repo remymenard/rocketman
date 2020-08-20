fd9bc081145b5f1630449af113f9ddeaf6693f7f
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-20 12:37:39 +0200

    can't book a rocket if no date

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,16 +7,24 @@ class OrdersController < ApplicationController
 
   def create
     @rocket = Rocket.find(params[:rocket_id])
-    begin_date = Date.iso8601(order_params[:begin_date])
-    end_date = Date.iso8601(order_params[:end_date])
-    @order = Order.new(rocket_id: @rocket.id,
-              renter_id: current_user.id,
-              begin_date: begin_date,
-              end_date: end_date,
-              total_price: @rocket.daily_price * (end_date - begin_date).to_i)
-    @order.save
 
-    redirect_to orders_path
+    unless (order_params[:begin_date].empty? || order_params[:end_date].empty?)
+      begin_date = Date.iso8601(order_params[:begin_date])
+      end_date = Date.iso8601(order_params[:end_date])
+      total_price = @rocket.daily_price * (end_date - begin_date).to_i
+    end
+
+    @order = Order.new(rocket_id: @rocket.id,
+                       renter_id: current_user.id,
+                       begin_date: begin_date,
+                       end_date: end_date,
+                       total_price: total_price)
+
+    if @order.save
+      redirect_to orders_path
+    else
+      redirect_to rocket_path(@rocket)
+    end
   end
 
   private
