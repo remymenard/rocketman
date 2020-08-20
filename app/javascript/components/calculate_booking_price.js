6bfbb4e64d5040647dcf7d53d93688a9f5efd16c
maelie poussier <maelie.poussier@icloud.com>
2020-08-20 16:20:09 +0200

    prix total

--- app/javascript/components/calculate_booking_price.js
+++
@@ -1,5 +0,0 @@
-// selectionner le start et le end date et le nombre de jours
-// transformer en date
-// soustraire date de fin et date de debut
-// multiplier par le nombre de jours
-// afficher a l'écran
---
+++ app/javascript/packs/calculate_booking_price.js
@@ -0,0 +1,22 @@
+// selectionner le start et le end date et le nombre de jours
+// transformer en date
+// soustraire date de fin et date de debut
+// multiplier par le nombre de jours
+// afficher a l'écran
+
+const begin_date = document.querySelector("#order_begin_date");
+const end_date = document.querySelector(".end_date");
+const daily_price = document.querySelector(".daily_price");
+const total_price = document.querySelector("#price");
+console.log(daily_price);
+
+const button = document.querySelector('#click-me');
+end_date.addEventListener('change', (event) => {
+
+  if (begin_date.value != null && end_date.value != null) {
+    var date1 = new Date(begin_date.value);
+    var date2 = new Date(end_date.value);
+    var Difference_In_Time = date2.getTime() - date1.getTime();
+    total_price.innerText = `Total price: ${(Difference_In_Time / (1000 * 3600 * 24))*daily_price.innerText} $`
+  }
+});
