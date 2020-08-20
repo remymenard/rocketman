0ce8c7ed9d2b095dc90b6a22fb693549f71a10bc
maelie poussier <maelie.poussier@icloud.com>
2020-08-20 12:27:11 +0200

    cancel order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,14 @@ class OrdersController < ApplicationController
     @rocket = Rocket.new
   end
 
+  def cancel
+    @rocket = Rocket.new
+    @order = Order.find(params[:id])
+    @order.destroy
+
+    redirect_to orders_path
+  end
+
   def show
   end
 
