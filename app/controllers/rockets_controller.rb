4589c14b96d69726f9323f36d6d3fcc599569af3
Pierre Waechter <pierre.waechter@audencia.com>
2020-08-20 15:33:18 +0200

    search bar operationnal but bad style

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -4,40 +4,48 @@ class RocketsController < ApplicationController
     @order = Order.new
   end
 
-  def index
-    @rockets = Rocket.all
-  end
-
   # def index
-  #   if !search_params[:continent].empty?
-  #     @rockets = Rocket.where(continent: search_params[:continent])
-  #   else
-  #     @rockets = Rocket.all
-  #   end
-
-  #   begin_date = search_params[:begin_date]
-  #   end_date = search_params[:end_date]
-
-  #   if (begin_date.empty? || end_date.empty? || begin_date == 'From' || end_date == 'To')
-  #     begin_date = nil
-  #     end_date   = nil
-  #   end
-
-  #   unless begin_date.nil? || end_date.nil?
-  #     @rockets.each do |rocket|
-  #       unless rocket.orders.empty?
-  #         rocket.orders.each do |order|
-  #           if order.status == 'Accepted'
-  #             unless (order.begin_date < search_params[:begin_date] && order.end_date < search_params[:begin_date]) || (order.begin_date > search_params[:end_date] && order.end_date > search_params[:end_date]
-  #               @rockets.delete(rocket))
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  #   @rockets
+  #   @rockets = Rocket.all
   # end
+
+  def index
+    if !search_params[:continent].empty?
+      @rockets_init = Rocket.where(continent: search_params[:continent])
+    else
+      @rockets_init = Rocket.all
+    end
+
+    @rockets = []
+    @rockets_init.each do |rocket_init|
+      @rockets << rocket_init
+    end
+
+    search_begin_date = search_params[:begin_date]
+    search_end_date = search_params[:end_date]
+
+    if (search_begin_date.empty? || search_end_date.empty? || search_begin_date == 'From' || search_end_date == 'To')
+      search_begin_date = nil
+      search_end_date   = nil
+    else
+      search_begin_date = Date.iso8601(search_begin_date)
+      search_end_date   = Date.iso8601(search_end_date)
+    end
+
+    unless search_begin_date.nil? || search_end_date.nil?
+      @rockets.each do |rocket|
+        unless rocket.orders.empty?
+          rocket.orders.each do |order|
+            if order.status == 'Accepted'
+              unless (order.begin_date < search_begin_date && order.end_date < search_begin_date) || (order.begin_date > search_end_date && order.end_date > search_end_date)
+                @rockets.delete(rocket)
+              end
+            end
+          end
+        end
+      end
+    end
+    @rockets
+  end
 
   private
 
