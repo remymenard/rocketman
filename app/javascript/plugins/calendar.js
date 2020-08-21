e8bb58cba7dccfb12e5989ff71f5670175409a7d
Remy Menard <remymenard.work@gmail.com>
2020-08-21 15:36:39 +0200

    trying to fix bug

--- app/javascript/plugins/calendar.js
+++ app/javascript/plugins/calendar.js
@@ -1,15 +1,17 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-document.addEventListener('turbolinks:load', function() {
+document.addEventListener('turbolinks:load', loadCalendar);
+function loadCalendar() {
   const calendarEl = document.getElementById('calendar');
   if (calendarEl != null) {
-  const calendar = new Calendar(calendarEl, {
+  let calendar = new Calendar(calendarEl, {
     events: '/owner/orders/index_json',
     height: 650,
     plugins: [ dayGridPlugin ],
-  initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth'
   });
   calendar.render();
 }
-});
+// calendarEl.fullCalendar('delete')
+}
